cac299e8f902c66aa52547800bca3e433c28e45a
Tom Montgomery <montgomery3681@gmail.com>
2024-06-26 22:58:52 -0600

    Initial Commit

---
+++ Networking_M1L5_Concurrency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Networking_M1L5_Concurrency
+//
+//  Created by tom montgomery on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Networking_M1L5_Concurrency/Networking_M1L5_ConcurrencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Networking_M1L5_ConcurrencyApp.swift
+//  Networking_M1L5_Concurrency
+//
+//  Created by tom montgomery on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct Networking_M1L5_ConcurrencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
