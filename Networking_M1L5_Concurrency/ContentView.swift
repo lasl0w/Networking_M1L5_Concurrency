fb3b16b77e88f9b873508b446c969f669622f355
Tom Montgomery <montgomery3681@gmail.com>
2024-06-27 22:43:19 -0600

    - concurrency example - async await - thread inspection + testing

--- Networking_M1L5_Concurrency/ContentView.swift
+++ Networking_M1L5_Concurrency/ContentView.swift
@@ -16,6 +16,25 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear() {
+            print("\(Thread.current) Start onAppear work")
+            // use a task block to invoke async work
+            Task {
+                await longTask()
+                print("\(Thread.current) After long task")
+                
+                await MainActor.run {
+                    // use MainActor.run if you need to force it back to the main thread
+                    print("Update some elements of the view")
+                }
+
+            }
+            print("\(Thread.current) Other non async work")
+        }
+    }
+    
+    func longTask() async {
+        print("\(Thread.current) Long Task")
     }
 }
 
